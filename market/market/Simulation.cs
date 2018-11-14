85c7bf48ae9e81338979d611c1dd94e94915e505
Thomaster194 <31244222+Thomaster194@users.noreply.github.com>
2018-11-14 15:58:00 +0100

    q

--- market/market/Simulation.cs
+++ market/market/Simulation.cs
@@ -18,7 +18,6 @@ namespace Market
             salesmen.Add(new Salesman("Thomaster194"));
             customers.Add(new Customer("Olav"));
             customers.Add(new Customer("Tord"));
-        }
-        
+        }   
     }
 }
