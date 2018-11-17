9cbdb103939df123a7e4cb6d889b8d1742ecd2a7
Unknown <tordjon@gmail.com>
2018-11-17 10:47:24 +0100

    Couple more person tests.

--- MarketTest/PersonTest.cs
+++ MarketTest/PersonTest.cs
@@ -1,3 +1,4 @@
+using System.Threading;
 using FleaMarket;
 using Item;
 using NUnit.Framework;
@@ -13,6 +14,28 @@ namespace MarketTest
             Person person = new Salesman("Petter");
             
             Assert.That(person.Name, Is.EqualTo("Petter"));
+        }
+
+        [Test]
+        public void personShouldHaveMoneyInWallet()
+        {
+            Person person = new Salesman("Kjell");
+            Assert.True(person.Wallet >= 100);
+        }
+
+        [Test]
+        public void personShouldLoseMoney()
+        {
+            Salesman salesman = new Salesman("Tor");
+            Customer customer = new Customer("Hans-Arne");
+            
+            customer.Wallet.Balance = 1000;
+            var previousAmount = customer.Wallet.Balance;
+            salesman.GetItems().Add(ItemFactory.GetRandomItem(salesman, 5));
+            salesman.SellItem();
+            Thread.Sleep(300);
+            
+            Assert.That(previousAmount, Is.GreaterThan(customer.Wallet.Balance));   
         }
     }
 }
